53b25816c7cafea61dcf46decd96e21e3b25c8d5
Yves Brissaud <yves.brissaud@gmail.com>
2025-02-26 10:27:12 +0100

    use constructor and expose `container`
    Signed-off-by: Yves Brissaud <yves.brissaud@gmail.com>

--- src/main/java/io/dagger/modules/jre/Jre.java
+++ src/main/java/io/dagger/modules/jre/Jre.java
@@ -13,26 +13,27 @@ public class Jre extends AbstractModule {
   private static final String jreDigest =
       "sha256:88593498863c64b43be16e8357a3c70ea475fc20a93bf1e07f4609213a357c87";
 
-  public Container jreContainer;
+  /** Internal container maintained by the Jre module */
+  public Container container;
 
-  /** Get a JRE container */
-  @Function
-  public Jre container() {
-    this.jreContainer = dag.container().from("%s@%s".formatted(jreImage, jreDigest));
-    return this;
+  public Jre() {}
+
+  public Jre(Client dag) {
+    super(dag);
+    this.container = dag.container().from("%s@%s".formatted(jreImage, jreDigest));
   }
 
   /** Copy JAR file to the container */
   @Function
   public Jre withJar(File jar) {
-    this.jreContainer = jreContainer.withFile("/opt/app.jar", jar);
+    this.container = container.withFile("/opt/app.jar", jar);
     return this;
   }
 
   /** Expose a network port. */
   @Function
   public Jre withExposedPort(int port) {
-    this.jreContainer = this.jreContainer.withExposedPort(port);
+    this.container = this.container.withExposedPort(port);
     return this;
   }
 
@@ -42,13 +43,13 @@ public class Jre extends AbstractModule {
     List<String> cmd = new java.util.ArrayList<>(List.of("java", "-jar"));
     args.ifPresent(strings -> cmd.addAll(List.of(strings)));
     cmd.add("/opt/app.jar");
-    return this.jreContainer.asService(new Container.AsServiceArguments().withArgs(cmd));
+    return this.container.asService(new Container.AsServiceArguments().withArgs(cmd));
   }
 
   /** Establish a runtime dependency on a service. */
   @Function
   public Jre withServiceBinding(String serviceName, Service service) {
-    this.jreContainer = this.jreContainer.withServiceBinding(serviceName, service);
+    this.container = this.container.withServiceBinding(serviceName, service);
     return this;
   }
 }
